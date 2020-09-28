6eac316f1b69be1a2275f339b34dc5e374f718c7
Chromos Planet <chromosplanet@gmail.com>
2020-09-28 12:51:53 -0300

    Add files via upload

---
+++ class-enviado_email.php
@@ -0,0 +1,145 @@
+<?php
+/**
+ * Class enviado_email file.
+ *
+ * @package WooCommerce\Emails
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+if ( ! class_exists( 'enviado_email', false ) ) :
+
+	/**
+	 * Customer Sent Order Email.
+	 *
+	 * Order sent emails are sent to the customer when the order is marked sent and usual indicates that the order has been shipped.
+	 *
+	 * @class       enviado_email
+	 * @version     2.0.0
+	 * @package     WooCommerce\Classes\Emails
+	 * @extends     WC_Email
+	 */
+	class enviado_email extends WC_Email {
+
+		/**
+		 * Constructor.
+		 */
+		public function __construct() {
+			$this->id             = 'customer_sent_order';
+			$this->customer_email = true;
+			$this->title          = __( 'Pedido enviado', 'woocommerce' );
+			$this->description    = __( 'Pedido enviado', 'woocommerce' );
+			$this->template_html  = 'emails/email-order-details.php';
+			$this->template_plain = 'emails/plain/email-order-details.php';
+			$this->placeholders   = array(
+				'{order_date}'   => '',
+				'{order_number}' => '',
+			);
+
+			// Triggers for this email.
+			add_action( 'woocommerce_order_status_changed', array( $this, 'trigger' ), 10, 2 );
+
+			// Call parent constructor.
+			parent::__construct();
+}
+
+		/**
+		 * Trigger the sending of this email.
+		 *
+		 * @param int            $order_id The order ID.
+		 * @param WC_Order|false $order Order object.
+		 */
+		public function trigger( $order_id, $order = false ) {
+			$this->setup_locale();
+if ( $order != 'sent' ){
+
+			if ( $order_id && ! is_a( $order, 'WC_Order' ) ) {
+				$order = wc_get_order( $order_id );
+			}
+
+			if ( is_a( $order, 'WC_Order' ) ) {
+				$this->object                         = $order;
+				$this->recipient                      = $this->object->get_billing_email();
+				$this->placeholders['{order_date}']   = wc_format_datetime( $this->object->get_date_created() );
+				$this->placeholders['{order_number}'] = $this->object->get_order_number();
+			}
+
+			if ( $this->is_enabled() && $this->get_recipient() ) {
+				$this->send( $this->get_recipient(), $this->get_subject(), $this->get_content(), $this->get_headers(), $this->get_attachments() );
+			}
+
+			$this->restore_locale();
+		}
+}
+		/**
+		 * Get email subject.
+		 *
+		 * @since  3.1.0
+		 * @return string
+		 */
+		public function get_default_subject() {
+			return __( 'Your {site_title} order is now sent', 'woocommerce' );
+		}
+
+		/**
+		 * Get email heading.
+		 *
+		 * @since  3.1.0
+		 * @return string
+		 */
+		public function get_default_heading() {
+			return __( 'Thanks for shopping with us', 'woocommerce' );
+		}
+
+		/**
+		 * Get content html.
+		 *
+		 * @return string
+		 */
+		public function get_content_html() {
+			return wc_get_template_html(
+				$this->template_html,
+				array(
+					'order'              => $this->object,
+					'email_heading'      => $this->get_heading(),
+					'additional_content' => $this->get_additional_content(),
+					'sent_to_admin'      => false,
+					'plain_text'         => false,
+					'email'              => $this,
+				)
+			);
+		}
+
+		/**
+		 * Get content plain.
+		 *
+		 * @return string
+		 */
+		public function get_content_plain() {
+			return wc_get_template_html(
+				$this->template_plain,
+				array(
+					'order'              => $this->object,
+					'email_heading'      => $this->get_heading(),
+					'additional_content' => $this->get_additional_content(),
+					'sent_to_admin'      => false,
+					'plain_text'         => true,
+					'email'              => $this,
+				)
+			);
+		}
+
+		/**
+		 * Default content to show below main email content.
+		 *
+		 * @since 3.7.0
+		 * @return string
+		 */
+		public function get_default_additional_content() {
+			return __( 'Thanks for shopping with us.', 'woocommerce' );
+		}
+	}
+
+endif;
